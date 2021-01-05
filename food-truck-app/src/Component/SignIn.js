1cc18de2090839f17572bd16d306ac3846ee0ef9
willmondal <wbmondal@aol.com>
2021-01-05 12:15:00 +0000

    added signin inputs with schema

--- food-truck-app/src/Component/SignIn.js
+++ food-truck-app/src/Component/SignIn.js
@@ -1,24 +1,115 @@
 import React, { useState, useEffect } from 'react';
+import * as yup from 'yup'
 
 // form schema
 import { signInFormSchema } from './FormSchema/signInFormSchema';
 
+
 // initial state
-const initialState = {};
+const initialDisabled = true
+const initialFormValues = {
+    username: "",
+    password: "",
+}
+const initialFormErrors = {
+    username: "",
+    password: "", 
+}
+
+const initialUsers = []
+
 
 const SignIn = () => {
 
     // component state
-    const [] = useState(initialState);
+    const [disabled, setDisabled] = useState(initialDisabled)
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [users, setUsers] = useState(initialUsers)
+
+
+    useEffect(()=>{
+        signInFormSchema.isValid(formValues).then((valid)=>{
+            setDisabled(!valid)
+        })
+    }, [formValues])
 
     // event handlers
 
+    const inputChange = (event) => {
+
+      event.preventDefault();
+
+     const {name, value} = event.target;
+        yup
+          .reach(signInFormSchema, name) 
+          .validate(value)
+          .then(() => {
+            setFormErrors({
+              ...formErrors,
+              [name]: "",
+            });
+          })
+          .catch((err) => {
+            setFormErrors({
+              ...formErrors,
+              [name]: err.errors[0],
+            });
+          });
+    
+        setFormValues({
+          ...formValues,
+          [name]: value, 
+        });
+      };
+
+    
+      const onSubmit = () => {
+        
+        const newUser = {
+            username: formValues.username,
+            password: formValues.password,
+        }
+
+        setUsers([...users, newUser])
+        setFormValues(initialFormValues)
+        
+      }
+
+      const submit = (event) =>{
+        event.preventDefault();
+        onSubmit();
+      }
+
+   
+
     return (
-        <div>
-            {/* build sign in form with:
-            - name
-            - password
-            */}
+        <div className='container'>
+        <form className='form container' onSubmit={submit}>
+            <h3>Sign In</h3>
+        <div className='form inputs'>
+        <label>
+            <span>Username:</span><input
+            placeholder='enter your Username'
+            name='username'
+            type='text'
+            onChange={inputChange}
+            />{formErrors.username}
+        </label>
+        <br/>
+        <label>
+            <span>Password:</span><input
+            placeholder='enter your Password'
+            name='password'
+            type='password'
+            onChange={inputChange}
+            />{formErrors.password}
+        </label>
+        </div>
+        <div className='form submit'>
+           <button disabled={disabled}>Sign In</button>
+        </div>
+        </form>
         </div>
     );
 
