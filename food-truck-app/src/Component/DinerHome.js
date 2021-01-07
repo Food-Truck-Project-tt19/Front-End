77a48e00d3ec362c24a331f08b892759c2637dc6
dylan-rinella <rinelladylan@aim.com>
2021-01-07 18:07:36 -0500

    styled diner and operator truck cards + Truck list title

--- food-truck-app/src/Component/DinerHome.js
+++ food-truck-app/src/Component/DinerHome.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import styled from 'styled-components'
 import { getTruckInfo } from '../Store/Actions';
 import mapStateToProps from '../Store/State';
 
 import DinerTruckCard from './DinerTruckCard';
+
+const StyledListTitle = styled.div`
+    @import url("https://fonts.googleapis.com/css?family=Oleo+Script:400|Open+Sans:300,300italic,600,600italic,800");
+
+    #title{
+            background: #ff4486;
+            color: #fff;
+			border-radius: 6px;
+            font-family: 'Oleo Script', serif;
+            text-align: center;
+			font-weight: 400;
+			padding: 0.05em 0.2em;
+			font-size: 2em;
+            letter-spacing: 0.05em;
+            box-shadow:2px 4px 3px rgba(0, 0, 0, 0.3);
+    }
+
+`
+
 
 const DinerHome = (props) => {
 
@@ -17,10 +36,10 @@ const DinerHome = (props) => {
     // event handlers
 
     return (
-        <div>
-            <h2>List of trucks</h2>
+        <StyledListTitle>
+            <h2 id='title'>List of trucks</h2>
                 {props.data.map(item => <DinerTruckCard key={item.id} {...item}/>)}
-        </div>
+        </StyledListTitle>
     );
 };
 
--- food-truck-app/src/Component/OperatorTruckCard.js
+++ food-truck-app/src/Component/OperatorTruckCard.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { getTruckInfo } from '../Store/Actions';
 import mapStateToProps from '../Store/State';
 
+const StyledTruck = styled.div`
+    display:flex;
+    border: .5px solid lightgrey;
+    flex-direction: column;
+    align-items: left;
+    background:#8EBCCD;
+    color: white;
+    padding: 1.4%;
+    margin-bottom: 2%;
+    text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.7);
+    box-shadow:2px 4px 3px rgba(0, 0, 0, 0.3);
+
+    p{
+        line-height: 1px;
+    }
+    h2{
+        line-height:2px;
+        /* color: #ff4486; */
+    }
+
+`
+
+
+
 const OperatorTruckCard = props => {
     return (
-        <div>
+        <StyledTruck>
             <h2>{props.name}</h2>
             <img src={props.truck.imageOfTruck} alt={props.name}/>
             <p>Cuisine: {props.truck.cuisineType}</p>
             <p>Current Location: {props.truck.truckLongitude} : {props.truck.truckLatitude}</p>
             <p>Departure Time: {props.truck.departureTime}</p>
-        </div>
+        </StyledTruck>
     );
 };
 
