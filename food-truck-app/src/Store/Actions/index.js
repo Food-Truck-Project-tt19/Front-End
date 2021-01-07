45ee81e4236f2cde5a486f15c44254dcf30f4316
cmirza <mail@cmirza.com>
2021-01-07 09:57:25 -0800

    fixed login issue

--- food-truck-app/src/Store/Actions/index.js
+++ food-truck-app/src/Store/Actions/index.js
@@ -45,25 +45,30 @@ export const logIn = signInData => dispatch => {
                 Authorization: `Basic ${btoa('lambda-client:lambda-secret')}`,
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
-        },
-        { timeout: 2 })
+        })
         .then(res => {
             console.log(res);
             window.localStorage.setItem('token', res.data.access_token);
+
+            axiosWithAuth().get('/users/getuserinfo/')
+            .then(res => {
+                console.log(res);
+                if (res.data.roles[0].role.name === 'DINER') {
+                    dispatch({ type: LOGIN_SUCCESS_DINER, payload: res.data });
+                } else {
+                    dispatch({ type: LOGIN_SUCCESS_OPERATOR, payload: res.data });
+                }
+            })
+            
+            .catch(err => {
+                console.log(err)
+            })
         })
         .catch(err => {
-            console.error(err);
+            console.log(err);
             dispatch({ type: LOGIN_FAIL, payload: err });
         });
-    axiosWithAuth().get('https://ccorvo-foodtruck-tracker-2021.herokuapp.com/users/getuserinfo')
-        .then(res => {
-            console.log(res);
-            if (res.data.roles[0].role.name === 'DINER') {
-                dispatch({ type: LOGIN_SUCCESS_DINER, payload: res.data });
-            } else {
-                dispatch({ type: LOGIN_SUCCESS_OPERATOR, payload: res.data });
-            }
-        })
+
 };
 
 // signup action diner
