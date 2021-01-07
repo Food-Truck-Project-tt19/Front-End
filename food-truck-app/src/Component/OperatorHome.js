db06b02346f8a62169234c774001732ec5d6376e
cmirza <mail@cmirza.com>
2021-01-06 19:54:57 -0800

    operator home sort of working

--- food-truck-app/src/Component/OperatorHome.js
+++ food-truck-app/src/Component/OperatorHome.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-const OperatorHome = () => {
+import { getTruckInfo } from '../Store/Actions';
+import mapStateToProps from '../Store/State';
+
+import OperatorTruckCard from './OperatorTruckCard';
+
+const OperatorHome = (props) => {
+
+    // fetch trucks
+    useEffect(() => {
+
+        console.log(props.data.mytrucklist)
+    }, []);
+
     return (
         <div>
-
+            <h2>Operator trucks</h2>
+            {props.data.mytrucklist.map(item => <OperatorTruckCard key={item.id} {...item}/>)}
         </div>
     );
 };
 
-export default OperatorHome;
+export default connect(mapStateToProps, { getTruckInfo })(OperatorHome);
