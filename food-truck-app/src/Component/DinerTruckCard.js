f6c51381e6360d27c3398aed712dc1e3706edd89
willmondal <wbmondal@aol.com>
2021-01-08 16:08:34 +0000

    same changes added to diner truck cards

--- food-truck-app/src/Component/DinerTruckCard.js
+++ food-truck-app/src/Component/DinerTruckCard.js
@@ -1,43 +1,76 @@
-import React from 'react';
-import styled from 'styled-components'
-import { connect } from 'react-redux';
-import { getTruckInfo } from '../Store/Actions';
-import mapStateToProps from '../Store/State';
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { getTruckInfo } from "../Store/Actions";
+import mapStateToProps from "../Store/State";
 
 const StyledTruck = styled.div`
-    display:flex;
-    border: .5px solid lightgrey;
+  display: flex;
+  border: 0.5px solid lightgrey;
+  flex-wrap: nowrap;
+  background: #8ebccd;
+  color: white;
+  padding: 1.4%;
+  margin-bottom: 2%;
+  text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.7);
+  box-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
+
+  p {
+    font-weight: 800;
+  }
+  h2 {
+    line-height: 2px;
+    /* color: #ff4486; */
+    margin-bottom: 6%;
+  }
+  span {
+    color: #ff4486;
+  }
+
+  img {
+    width: 80%;
+    border-radius: 20px;
+    border: 4px dashed #ff4486;
+    margin-left: 3%;
+  }
+
+  .info {
+    width: 80%;
+    margin-top: 5%;
+    display: flex;
     flex-direction: column;
-    align-items: left;
-    background:#8EBCCD;
-    color: white;
-    padding: 1.4%;
-    margin-bottom: 2%;
-    text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.7);
-    box-shadow:2px 4px 3px rgba(0, 0, 0, 0.3);
+  }
+  .picture {
+    width: 80%;
+  }
+`;
 
-    p{
-        line-height: 1px;
-    }
-    h2{
-        line-height:2px;
-        /* color: #ff4486; */
-    }
-
-`
-
-
-const DinerTruckCard = props => {
-
-    return (
-        <StyledTruck>
-            <h2>{props.name}</h2>
-            <img src={props.imageOfTruck} alt={props.name}/>
-            <p>Cuisine: {props.cuisineType}</p>
-            <p>Current Location: {props.truckLongitude} : {props.truckLatitude}</p>
-            <p>Departure Time: {props.departureTime}</p>
-        </StyledTruck>
-    );
+const DinerTruckCard = (props) => {
+  return (
+    <StyledTruck>
+      <div className="picture">
+        <h2>{props.name}</h2>
+        <img src={props.imageOfTruck} alt={props.name} />
+      </div>
+      <div className="info">
+        <p>
+          Name: <span>{props.name}</span>
+        </p>
+        <p>
+          Cuisine: <span>{props.cuisineType}</span>
+        </p>
+        <p>
+          Current Location:{" "}
+          <span>
+            {props.truckLongitude} : {props.truckLatitude}
+          </span>
+        </p>
+        <p>
+          Departure Time: <span>{props.departureTime}</span>
+        </p>
+      </div>
+    </StyledTruck>
+  );
 };
 
 export default connect(mapStateToProps, { getTruckInfo })(DinerTruckCard);
